61559d795d8a81efd96ee80751725d18a7358ae8
DAVID EZEQUIEL WEISSBROD <47436755@ort>
2024-05-22 09:00:59 -0300

    cosas del event_controller

--- src/controllers/event_controller.js
+++ src/controllers/event_controller.js
@@ -3,7 +3,7 @@ import EventService from 'src/services/event_service.js'
 const router = Router();
 const svc = new EventService();
 
-router.get('', async (req, res) =>{
+router.get('/api/event/', async (req, res) =>{
     let res;
     const array = await svc.getAllAsync();
     if(array != null){
@@ -13,3 +13,54 @@ router.get('', async (req, res) =>{
     }
     return res;
 })
+
+router.get('/api/event/?name={texto}', async (req, res) => {
+    const array = [];
+    array = await svc.getEventByName(req.params.name)
+    if(array != null){
+        array = res.status(200).json(array)
+    } else{
+        array = res.status(500).send('Error')
+    }
+})
+
+router.get('/api/event/?category={texto}', async (req, res) => {
+    const array = [];
+    array = await svc.getEventByCategory(req.params.category)
+    if(array != null){
+        array = res.status(200).json(array)
+    } else{
+        array = res.status(500).send('Error')
+    }
+})
+
+router.get('/api/event/?startdate={fecha YYYY-MM-DD}', async (req, res) => {
+    const array = [];
+    array = await svc.getEventByDate(req.params.startdate)
+    if(array != null){
+        array = res.status(200).json(array)
+    } else{
+        array = res.status(500).send('Error')
+    }
+})
+
+router.get('/api/event/?tag={texto}', async (req, res) => {
+    const array = [];
+    array = await svc.getEventByTag(req.params.tag)
+    if(array != null){
+        array = res.status(200).json(array)
+    } else{
+        array = res.status(500).send('Error')
+    }
+})
+
+router.get('/api/event/{id}', async (req, res) => {
+    const array = [];
+    array = await svc.getEventById(req.params.id)
+    if(array != null){
+        array = res.status(200).json(array)
+    } else{
+        array = res.status(404).send('No se encontro el id')
+    }
+})
+
