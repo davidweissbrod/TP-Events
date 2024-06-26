642b887309fb62610d2bf2cbc26b396ccb3212e8
DAVID EZEQUIEL WEISSBROD <47436755@ort>
2024-06-26 14:06:37 -0300

    cosas

--- src/servicies/event_location_service.js
+++ src/servicies/event_location_service.js
@@ -1,4 +1,5 @@
 import EventLocationRepository from '../repositories/event_location_repository.js'
+import AuthService from '../middlewares/auth_middleware.js'
 
 export default class EventLocationService {
     getAllAsync = async () => {
@@ -6,23 +7,77 @@ export default class EventLocationService {
         return await repo.getAllAsync();
     }
 
-    getEventLocationById = async (id) => {
-        const repo = new EventLocationRepository();
-        return await repo.getEventLocationById(id);
+    getEventLocationById = async (id_creator_user) => {
+        let objeto = {
+            success: false,
+            message: "",
+            token: ""
+        }     
+        const repo = new EventLocationRepository()
+        const auth = new AuthService()
+        let user = await repo.getUserById(id_creator_user)
+        if(user != null){
+            objeto.success = true;
+            objeto.message = "Correcto";
+            objeto.token = await auth.login(user);
+        } else{
+            objeto.message = "No se encontro el usuario";
+        }
+        return await repo.getEventLocationById(id_creator_user);
     }
 
     insertEventLocation = async (evLoc) => {
+        let objeto = {
+            success: false,
+            message: "",
+            token: ""
+        }
         const repo = new EventLocationRepository();
+        const auth = new AuthService();
+        let user = await repo.getUserById(evLoc.id_creator_user)
+        if(user != null){
+            objeto.success = true;
+            objeto.message = "Correcto";
+            objeto.token = await auth.generateToken(objeto);
+        } else{
+            objeto.message = "No se encontro el usuario";
+        }     
         return await repo.insertEventLocation(evLoc);
     }
 
     updateEventLocation = async (evLoc) => {
+        let objeto = {
+            success: false,
+            message: "",
+            token: ""
+        }
         const repo = new EventLocationRepository();
+        const auth = new AuthService();
+        let user = await repo.getUserById(evLoc.id_creator_user)
+        if(user != null){
+            objeto.success = true
+            objeto.message = "Correcto"
+            objeto.token = await auth.generateToken(objeto);
+        }
         return await repo.updateEventLocation(evLoc);
     }
 
-    deleteEventLocation = async (id) => {
-        const repo = new EventLocationRepository();
-        return await repo.deleteEventLocation(id)
+    deleteEventLocation = async (id_creator_user) => {
+        let objeto = {
+            success: false,
+            message: "",
+            token: ""
+        }     
+        const repo = new EventLocationRepository()
+        const auth = new AuthService()
+        let user = await repo.getUserById(id_creator_user)
+        if(user != null){
+            objeto.success = true;
+            objeto.message = "Correcto";
+            objeto.token = await auth.login(user);
+        } else{
+            objeto.message = "No se encontro el usuario";
+        }
+        return await repo.deleteEventLocation(id_creator_user)
     }
 }
